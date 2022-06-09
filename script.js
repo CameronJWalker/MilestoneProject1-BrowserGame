bc651a49b70e0991f4de8301eb50a02a826bccde
CameronJWalker <cameron.jr.walker@icloud.com>
2022-06-08 22:22:31 -0700

    added some questions

---
+++ script.js
@@ -0,0 +1,39 @@
+const questions = [
+    {
+        question: "Who wields the dark side and want to rule the galaxy?",
+        answers: {
+            a: 'The Jedi',
+            b: 'Jar Jar Binks',
+            c: 'The Clone Troopers',
+            d: 'The Sith'
+        },
+
+    correctAnswer: "d"
+    
+    },
+    {
+        question: "What is baby yodas real name?",
+        answers: {
+            a: 'Yoda',
+            b: 'Gragah',
+            c: 'Grogu',
+            d: 'Palpatine'
+        },
+
+    correctAnswer: "c"
+    
+    },
+    {
+        question: "Who is Palpatine's granddaughter?",
+        answers: {
+            a: 'Rey',
+            b: 'Padme',
+            c: 'Leia',
+            d: 'Satine'
+        },
+
+    correctAnswer: "a"
+    
+    }
+    
+]
