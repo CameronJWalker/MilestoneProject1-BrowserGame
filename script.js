f4f574a479c61bb5e27a511128dbd392f3a9059c
CameronJWalker <cameron.jr.walker@icloud.com>
2022-06-09 15:47:33 -0700

    added more questions

--- script.js
+++ script.js
@@ -34,6 +34,53 @@ const questions = [
 
     correctAnswer: "a"
     
-    }
+    },
+    {
+        question: "Who adopted Anakin and Padme's daughter?",
+        answers: {
+            a: 'Bail Organa',
+            b: 'The Mandalorian',
+            c: 'Mace Windu',
+            d: 'Palpatine'
+        },
+
+    correctAnswer: "a"
     
+    },
+    {
+        question: "What color was Mace Windu's lightsaber?",
+        answers: {
+            a: 'Green',
+            b: 'Blue',
+            c: 'Red',
+            d: 'Purple'
+        },
+
+    correctAnswer: "d"
+    
+    },
+    {
+        question: "What was Galen Erso's nickname for his daughter?",
+        answers: {
+            a: 'Stardust',
+            b: 'Sunshine',
+            c: 'Hyperdrive',
+            d: 'Daughter'
+        },
+
+    correctAnswer: "a"
+    
+    },
+    {
+        question: "Who does BB-8 belong to?",
+        answers: {
+            a: 'Anakin Skywalker',
+            b: 'Poe Dameron',
+            c: 'Princess Leia',
+            d: 'Luke Skywalker'
+        },
+
+    correctAnswer: "b"
+    
+    },
 ]
